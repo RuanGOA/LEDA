448885e21c61071ad0584e580c5123a5a0983d8a
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2019-08-29 11:46:28 -0300

    .

---
+++ estudo/Order/src/alg/QuickSort.java
@@ -0,0 +1,16 @@
+package alg;
+
+import util.Util;
+
+public class QuickSort {
+	
+	static Util util = new Util();
+	
+	public static void main(String args[]) {
+		
+		
+		util.exibir(vetor);
+	}
+	
+	
+}
