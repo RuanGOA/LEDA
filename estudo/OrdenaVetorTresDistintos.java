1684be141905236a7bf2bcdf4cdcc716f4db5f1a
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2019-09-17 22:15:22 -0300

    se agora n foi, eu desisto

--- estudo/OrdenaVetorTresDistintos.java
+++ estudo/OrdenaVetorTresDistintos.java
@@ -4,7 +4,7 @@ public class OrdenaVetorTresDistintos {
 	
 	public static void main(String[] args) {
 		
-		int[] arr = new int[] {3, 1, 2, 1, 3, 1, 2, 2, 1, 3, 2};
+		int[] arr = new int[] {1, 2, 3, 3, 1, 2, 3, 1, 1, 2, 3, 1, 3, 1, 1, 2, 3, 1, 2, 3, 2, 3, 1 , 2, 3, 3};
 		
 		sort(arr);
 		
@@ -18,19 +18,22 @@ public class OrdenaVetorTresDistintos {
 	private static void sort(int[] arr) {
 		
 		int min = 100000; int max = -1;
-		for(int i = 0; i < arr.length; i++) { //acha min e maximo
+		for(int i = 0; i < arr.length; i++) { 
 			if(arr[i] > max) max = arr[i];
 			if(arr[i] < min) min = arr[i];
 		}
 		
-		int i = 0; int m = 1; int j = arr.length - 1; //i inicio, m pivot, j final
-		while(m < j && i < j) { // quando o ponteiro m alcancar o j, ou o i alcancar o j, o array estara ordenado
-			if(arr[i] == min) i++; //se o i ja for o min, avance
-			if(arr[j] == max) j--; //se o j ja for o max, regrida
-			
-			if(arr[m] == min) swap(arr, i++, m); //se o pivot for igual ao min, troque com o i, e o avance
-			else if(arr[m] == max) swap(arr, j--, m); //se o pivot for igual ao max, troque com o j, e o regrida
-			else m++; //se nem for o min, nem o max, avance o pivot
+		
+		int i = 0; int m = 1; int j = arr.length - 1; 
+		while(m < j) { 
+			if(arr[i] == min) { //para evitar o i = m, se o i for igual a min, ou i > m, sempre que o i == m -> m = i + 1
+				i++;
+				if(i == m) m++;
+			} else if(arr[j] == max) j--; //se o j for igual a max, regrida um
+			else if(arr[i] == max || arr[j] == min) swap(arr, i, j); //se o i ou j for igual a max ou min respectivamente, troque
+			else if(arr[m] == min) swap(arr, i, m); //se o m for min, troque com i
+			else if(arr[m] == max) swap(arr, j, m); //se o m for max, troque com j
+			else m++; //se n for nem min, nem j, eh o valor intermediario, entao aumente 1
 		}
 	}
 	
