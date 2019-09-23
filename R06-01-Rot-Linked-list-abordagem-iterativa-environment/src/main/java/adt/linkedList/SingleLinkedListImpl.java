785cacbe51d4d04c27001ab128aebb80906fe2c2
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2019-09-23 17:46:20 -0300

    roteiro 6 full gambiarra/incompleto

---
+++ R06-01-Rot-Linked-list-abordagem-iterativa-environment/src/main/java/adt/linkedList/SingleLinkedListImpl.java
@@ -0,0 +1,71 @@
+package adt.linkedList;
+
+public class SingleLinkedListImpl<T> implements LinkedList<T> {
+
+	protected SingleLinkedListNode<T> head;
+
+	public SingleLinkedListImpl() {
+		this.head = new SingleLinkedListNode<T>();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return this.head.isNIL();
+	}
+
+	@Override
+	public int size() {
+		int cont = 0;
+		if(!isEmpty()) {
+			SingleLinkedListNode node = new SingleLinkedListNode<T>();
+			while(!node.isNIL()) {
+				cont++;
+				node = node.next;
+			}
+		}
+		return cont;
+	}
+
+	@Override
+	public T search(T element) {
+		T resultado = null;
+		if(!isEmpty()) {
+			SingleLinkedListNode node = new SingleLinkedListNode<T>();
+			while(!node.isNIL()) {
+				if(node.data.equals(element)) {
+					resultado = (T) node.data;
+					break;
+				} else node = node.next;
+			}
+		}
+		
+		return resultado;
+	}
+
+	@Override
+	public void insert(T element) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented!");
+	}
+
+	@Override
+	public void remove(T element) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented!");
+	}
+
+	@Override
+	public T[] toArray() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented!");
+	}
+
+	public SingleLinkedListNode<T> getHead() {
+		return head;
+	}
+
+	public void setHead(SingleLinkedListNode<T> head) {
+		this.head = head;
+	}
+
+}
